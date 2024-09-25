03fa96fa6966ed8ad34c55085123914b2d163e1b
RushikeshKokate22 <rushikesh.kokate@tradebrains.in>
2024-09-25 11:24:07 +0530

    latest UI fixes for SpotNow page

--- src/app/(pages)/spotnow/_components/Power.tsx
+++ src/app/(pages)/spotnow/_components/Power.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { IMAGES } from "@/assets";
 import Image from "next/image";
+import Title from './Title';
 
 const data = [
     {
@@ -39,17 +40,18 @@ const Power = () => {
     return (
         <div className='bg-[#EEFDF6]'>
             <div className='flex flex-col'>
-                <div className='flex justify-center text-center'>
-                    <span className='px-2 max-xl:text-3xl max-md:text-2xl max-2xl:text-[43px]  font-roboto text-6xl text-home-heading font-900 pt-28 pb-10'>
-                        Power your Field Service Team and Deliver Better Results
-                    </span>
+                <div className='max-2xl:pt-20'>
+                 <Title
+                    title="Power your Field Service Team and Deliver Better Results"
+                    description=" SpotNow is your key to seamless, agile, and data-driven workforce management."
+                    descriptionWidth='max-2xl:w-[936px]'
+                    titleWidth = 'max-2xl:w-[752px] max-lg:w-[752px] 2xl:mt-28'
+                />
                 </div>
-                <span className='font-500 font-roboto-serif text-3xl max-lg:text-xl text-center max-2xl:text-[17px] 2xl:text-[22px] max-md:text-[17px] max-md:leading-tight'>
-                    SpotNow is your key to seamless, agile, and data-driven workforce management.
-                </span>
+                
                 <div className='flex justify-center flex-wrap gap-10 py-16'>
                     {data.map((item, index) => (
-                        <div key={index} className='bg-white w-[27vw] max-lg:w-[40vw] max-md:w-[80vw] py-10 px-4 shadow-2xl rounded-xl'>
+                        <div key={index} className='bg-white w-[27vw] max-lg:w-[40vw] max-md:w-[80vw] py-10 px-4 shadow-xl rounded-xl'>
                             <div className='flex justify-center py-4'>
                                 <Image src={item.imgSrc} alt='brand' />
                             </div>
--- src/app/(pages)/spotnow/_components/Tracking.tsx
+++ src/app/(pages)/spotnow/_components/Tracking.tsx
@@ -1,20 +1,23 @@
 import { IMAGES } from '@/assets'
 import Image from 'next/image'
 import React from 'react'
+import Title from './Title'
 
 const Tracking = () => {
     return (
         <div className=' bg-[#E6F0FF] text-center py-16'>
+            <Title 
+            title='Field Employee Tracking Software' 
+            descriptionWidth='max-w-[935px] 2xl:max-w-[1339px]'
+            description='Empower your business with SpotNow, the cutting-edge cloud-based Field Employee Tracking App. Effortlessly monitor daily work activities, enhance productivity, and gain real-time insights into your remote workforce teams. Perfect for sales, support, and beyond – SpotNow simplifies, streamlines, and supercharges your operations.'
+            />
             <div className=' flex flex-col'>
-                <span className=' font-900 font-roboto text-6xl max-md:text-3xl text-home-heading max-2xl:text-[43px]'>Field Employee Tracking Software</span>
-                <div className='flex justify-center'>
-                    <span className=' w-[75vw] font-500 max-md:w-full max-lg:px-2 font-roboto-serif text-2xl max-md:text-base max-2xl:text-[17px]  2xl:text-[22px] pt-6'>Empower your business with SpotNow, the cutting-edge cloud-based Field Employee Tracking App. Effortlessly monitor daily work activities, enhance productivity, and gain real-time insights into your remote workforce teams. Perfect for sales, support, and beyond – SpotNow simplifies, streamlines, and supercharges your operations.</span>
-                </div>
+                 
                 <div className=' flex justify-center'>
                 <Image src={IMAGES.spotReactangle} alt='' className='w-[90vw] pt-10' />
                 </div>
-            </div>
-            <div>
+               </div>  
+              <div>
             </div>
         </div>
     )
