edc1e3b4a9932664161dcd221855ff58fbf3e296
Gajendra kumar ravidas <ravidasgajendra1425@gmail.com>
2024-09-28 11:44:14 +0530

    gif issue is fixed

--- src/app/(pages)/googleworkspace/_components/Transforming.tsx
+++ src/app/(pages)/googleworkspace/_components/Transforming.tsx
@@ -7,8 +7,8 @@ const Transforming = () => {
         <div className='bg-[#F1FFF6D9]'>
             <div className='flex justify-center py-24 max-md:py-6'>
                 <div className='flex flex-col gap-6  text-center w-[70vw]  max-lg:w-[96vw] '>
-                    <span className=' text-6xl max-lg:text-4xl max-md:text-xl font-roboto max-md:tracking-tighter font-900  text-home-heading'>Google Workspace: Transforming Collaboration and Productivity</span>
-                    <span className=' font-roboto-serif font-400 text-2xl max-md:text-sm'>Want to know about Google Workspace Services At Your Fingertips.</span>
+                    <span className=' 2xl:text-[64px] md:text-[43px] max-md:text-[26px] 2xl:leading-[76.8px] md:leading-[51.6px] max-md:leading-[31.2px] font-roboto max-md:tracking-tighter font-900  text-home-heading'>Google Workspace: Transforming Collaboration and Productivity</span>
+                    <span className=' font-roboto-serif font-400 2xl:text-[22px] 2xl:leading-[36.3px] max-2xl:text-[17px] max-2xl:leading-[28.05px] '>Want to know about Google Workspace Services At Your Fingertips.</span>
                 </div>
             </div>
             <div className=' flex justify-center pb-20'>
