1cd955a8362db2e63d8b68f0067fcdff145d67c5
Rahul Sutradhar <rahulsd380@gmail.com>
2024-10-01 10:22:27 +0600

    fixed responisve issue in homepage

--- src/app/(pages)/(home)/_components/Organization.tsx
+++ src/app/(pages)/(home)/_components/Organization.tsx
@@ -70,12 +70,12 @@ type CardProps = {
 };
 
 const Card: React.FC<CardProps> = ({ title, description, imgSrc }) => (
-  <div className='bg-[#FFFBF0] w-full max-2xl:text-rounded-[10px] max-2xl:w-[275px] max-2xl:h-[275px] group pt-4 hover:scale-105  p-4 hover:border-[#0011FF] hover:border-[2px] duration-300'>
+  <div className='bg-[#FFFBF0] max-2xl:text-rounded-[10px] max-2xl:h-[275px] group pt-4 hover:scale-105 p-4 hover:border-[#0011FF] hover:border-[2px] duration-300'>
     <Image src={imgSrc} alt={title} className="" />
-    <div className="flex gap-4 mt-3  max-2xl:w-[251px]">
-      <span className="text-[22px] max-sm:text-[18px] max-2xl:text-[24px] 2xl:text-[26px] text-home-heading leading-[28.13px] tracking-tighter font-900 w-[300px] h-[56px] max-lg:text-[24px]">{title}</span>
+    <div className="flex gap-4 mt-3">
+      <span className="text-[22px] max-sm:text-[18px] max-2xl:text-[24px] 2xl:text-[26px] text-home-heading leading-[28.13px] tracking-tighter font-900 max-lg:text-[24px]">{title}</span>
     </div>
-    <div className=" max-2xl:pt-4 text-[#000334] font-serif tracking-tighter max-2xl:text-[15px] hover:opacity-60 mt-3 group text-[17px] 2xl:text-[20px]  font-400 w-[280px] max-lg:text-[15px] max-2xl:w-[200px]">
+    <div className=" text-[#000334] font-serif tracking-tighter max-2xl:text-[15px] hover:opacity-60 mt-2 group text-[17px] 2xl:text-[20px]  font-400 w-[280px] max-lg:text-[15px] max-2xl:w-[200px]">
       <span>{description}</span>
     </div>
   </div>
@@ -107,7 +107,7 @@ const Organization: React.FC = () => {
             ))}
           </div>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-10 mx-20 max-2xl:mx-8 max-lg:pb-[3rem] ">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-10 px-5 md:px-4 xl:px-[64px] w-full">
           {cardData.map((card, index) => (
             <Card
               key={index}
