52549b5f3cebcd54e60f1c2b59aedeb10e2c4072
Rahul Sutradhar <rahulsd380@gmail.com>
2024-10-03 12:18:03 +0600

    Build section fixed

--- src/app/(pages)/domain/_components/Perfectdomain.tsx
+++ src/app/(pages)/domain/_components/Perfectdomain.tsx
@@ -17,7 +17,7 @@ const DomainCard: React.FC<DomainCardProps> = ({ image, title, price, originalPr
   <div
     onMouseEnter={() => setHoveredCard(index)}
     onMouseLeave={() => setHoveredCard(null)}
-    className={`relative hover:scale-105 w-[276px] md:w-[277.368px] xl:w-[276px] 3xl:w-[400px] group shadow-[0px_2px_2px_0px_#00000040] h-[200px] rounded-[10px] p-[2px] transition-all duration-300 ${hoveredCard === index ? 'bg-gradient-to-r from-blue-500 to-yellow-500' : 'bg-transparent'}`}
+    className={`relative hover:scale-105 w-full max-w-[400px] group shadow-[0px_2px_2px_0px_#00000040] h-[200px] rounded-[10px] p-[2px] transition-all duration-300 ${hoveredCard === index ? 'bg-gradient-to-r from-blue-500 to-yellow-500' : 'bg-transparent'}`}
     style={{ background: hoveredCard === index ? 'linear-gradient(0deg, rgba(233,45,253,1) 0%, rgba(136,114,226,1) 55%)' : 'transparent' }}
   >
     <div className='relative bg-white py-5 px-4 rounded-[10px] h-[197px] flex flex-col gap-4'>
@@ -62,18 +62,19 @@ const Perfectdomain: React.FC = () => {
       <div className='flex flex-col items-center justify-center'>
           
         {/* Build your brand with the perfect domain name span */}
-        <span className="text-center font-900 font-roboto text-home-heading z-10 text-[38px] md:text-[43px] 2xl:text-[64px] leading-[45.6px] md:leading-[51.6px] 2xl:leading-[76.8px] px-2 sm:px-0 tracking-tight max-w-[323px] md:max-w-[677px] xl:max-w-[500px] 3xl:max-w-[1367px] mx-auto">
-          Build your brand with the perfect domain name
-        </span>
+        <span className="text-center font-900 font-roboto text-home-heading z-10 text-[38px] max-md:text-[43px] max-2xl:text-[43px] max-3xl:text-[64px] leading-[45.6px] md:leading-[51.6px] 2xl:leading-[76.8px] px-2 sm:px-0 tracking-tight max-3xl:max-w-full max-1xl:max-w-[500px] max-lg:max-w-[677px] max-sm:max-w-[323px] max-w-full  mx-auto">
+  Build your brand with the perfect domain name
+</span>
+
         
         {/* With more than 300 domain extensions span */}
-        <span className="text-center text-[17px] 2xl:text-[22px] leading-[28.5px] 2xl:leading-[36.3px] mt-3 md:mt-5 xl:mt-4 3xl:mt-[30px] font-500 font-roboto-serif tracking-tight max-w-[286px] md:max-w-[617px] xl:max-w-[290px] 3xl:max-w-[938px] mx-auto z-10 px-4 sm:px-0">
+        <span className="text-center text-[17px] 2xl:text-[22px] leading-[28.5px] 2xl:leading-[36.3px] mt-3 md:mt-5 xl:mt-4 3xl:mt-[30px] font-500 font-roboto-serif tracking-tight max-w-full max-sm:max-w-[286px] max-lg:max-w-[460px] max-1xl:max-w-[290px] max-3xl:max-w-[938px] mx-auto z-10 px-4 sm:px-0">
           With more than 300 domain extensions, you&apos;ll find the one that fits just right.
         </span>
       </div>
       
       <div className='flex justify-center'>
-        <div className='grid grid-cols-4 max-xl:grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-1 gap-4 md:gap-[35.63px] xl:gap-[17px] 3xl:gap-[39.91px]'>
+        <div className='grid grid-cols-4 max-xl:grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-1 gap-4 md:gap-[35.63px] xl:gap-[17px] 3xl:gap-[39.91px] w-full px-[20px] md:px-[90px] lg:px-[75px] 3xl:px-[100px]'>
           {domainDetails.map((domain, index) => (
             <DomainCard
               key={index}
